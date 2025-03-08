d27a32290b0b2e39a98dfe4ffb3d2b80aa7180e4
nsalkarka <_karka67@windowslive.com>
2025-03-08 21:09:21 +0300

    Add project files.

---
+++ TravelTripProje/Controllers/DefaultController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        Context c=new Context();
+        public ActionResult Index()
+        {
+           
+            var degerler= c.Blogs.Take(4).ToList();
+            return View(degerler);
+        }
+
+        public PartialViewResult Partial3()
+        {
+            var deger=c.Blogs.Take(10).ToList();
+            return PartialView(deger);
+        }
+
+        public PartialViewResult Partial4()
+        {
+            var deger=c.Blogs.Take(3).ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial5()
+        {
+            var deger = c.Blogs.Take(3).OrderByDescending(x=>x.ID).ToList();
+            return PartialView(deger);
+        }
+
+
+    }
+}
